8d96fc029809a74dae57e8e37cecd20af0b81535
ahmad655656 <ahmadsalloum8899@gmail.com>
2025-05-12 02:26:36 +0300

    added

---
+++ src/api/categore/routes/categore.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * categore router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::categore.categore');
---
+++ src/api/categore/services/categore.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * categore service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::categore.categore');
